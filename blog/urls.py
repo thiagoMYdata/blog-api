2de7bb45006d8e6f996478886eab423a8d140625
Thiago Blanco <thiago.dataislife@gmail.com>
2026-07-13 16:36:15 -0300

    blog CRUD, just api

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import APIListPost
+from .views import APIListPost,APIDestoryPost,APIDetailPost,APIUpdatePost,APICreatePost
 
 urlpatterns = [
     path("", APIListPost.as_view()),
+    path("get/<int:pk>/", APIDetailPost.as_view()),
+    path("put/<int:pk>/", APIUpdatePost.as_view()),
+    path("delete/<int:pk>/", APIDestoryPost.as_view()),
+    path("post/", APICreatePost.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,22 @@ from .models import Post
 from .serializers import PostSerializer
 
 # Create your views here.
-class APIListPost(generics.ListAPIView):
+class APIListPost(generics.ListAPIView):    # GET all
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class APIDetailPost(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class APIUpdatePost(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class APIDestoryPost(generics.DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class APICreatePost(generics.CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
